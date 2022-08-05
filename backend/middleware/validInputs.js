5e4679bc17f16be4058086e5600d7ac3958d5452
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-05 13:35:28 +0200

    Middleware to check if password is long enough

--- backend/middleware/validInputs.js
+++ backend/middleware/validInputs.js
@@ -10,3 +10,15 @@ exports.validEmail = async (req, res, next) => {
     return res.status(500).send({ error: error.message });
   }
 };
+
+exports.validPassword = async (req, res, next) => {
+  try {
+    if (req.body.password.length >= 8) {
+      next();
+    } else {
+      return res.status(401).json({ error: "Le mot de passe doit contenir au moins 8 caractères" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const authCtrl = require("../controllers/auth");
 const validInputs = require("../middleware/validInputs");
 
-router.post("/register", validInputs.validEmail, authCtrl.userRegister);
+router.post("/register", validInputs.validEmail, validInputs.validPassword, authCtrl.userRegister);
 router.post("/login", authCtrl.userLogin);
 
 module.exports = router;
