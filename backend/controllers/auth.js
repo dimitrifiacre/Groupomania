ed45c7f0d119130751d3e88038484d6341019621
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-05 11:37:08 +0200

    Adding function for user registration

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,2 +1,22 @@
-exports.userRegister = async (req, res) => {};
+const db = require("../models");
+const bcrypt = require("bcrypt");
+
+exports.userRegister = async (req, res) => {
+  try {
+    const emailExist = await db.User.findOne({ where: { user_email: req.body.email } });
+    if (!emailExist) {
+      const user = await db.User.create({
+        user_firstname: req.body.firstname,
+        user_lastname: req.body.lastname,
+        user_email: req.body.email,
+        user_password: bcrypt.hashSync(req.body.password, 10),
+      });
+      return res.status(201).send(user);
+    } else {
+      return res.status(401).json({ error: "L'adresse email est déjà utilisée" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
 exports.userLogin = async (req, res) => {};
