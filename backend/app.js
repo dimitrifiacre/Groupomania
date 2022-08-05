a1ddb48b9ece5d2a3767ef45be4982791a07b8a2
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-05 10:17:06 +0200

    Installation and setup of Sequelize ORM

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+// Connexion à la db
+const { sequelize } = require("./models");
+sequelize
+  .authenticate()
+  .then(() => console.log("Connexion à la base de donnée réussie"))
+  .catch((error) => console.log("Connexion à la base de donnée échouée : " + error));
+
+// Écoute sur un port
 const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
   console.log(`Le serveur a été lancé sur le port ${port}`);
 });
-
-module.exports = app;
