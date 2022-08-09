98215da03d85c94c695cb1be09de347cb7112504
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-09 14:11:45 +0200

    Changing way modules are imported into controllers

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const authCtrl = require("../controllers/auth");
-const validInputs = require("../middleware/validInputs");
+const { userRegister, userLogin, userLogout } = require("../controllers/auth");
+const { validPassword } = require("../middleware/validInputs");
 
-router.post("/register", validInputs.validPassword, authCtrl.userRegister);
-router.post("/login", authCtrl.userLogin);
-router.post("/logout", authCtrl.userLogout);
+router.post("/register", validPassword, userRegister);
+router.post("/login", userLogin);
+router.post("/logout", userLogout);
 
 module.exports = router;
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const postCtrl = require("../controllers/post");
+const { getAllPosts, createPost, updatePost, deletePost } = require("../controllers/post");
 const auth = require("../middleware/authJwt");
 const multer = require("../middleware/multerConfig");
 
-router.get("/", auth, postCtrl.getAllPosts);
-router.post("/", auth, multer, postCtrl.createPost);
-router.put("/:id", auth, multer, postCtrl.updatePost);
-router.delete("/:id", auth, postCtrl.deletePost);
+router.get("/", auth, getAllPosts);
+router.post("/", auth, multer, createPost);
+router.put("/:id", auth, multer, updatePost);
+router.delete("/:id", auth, deletePost);
 
 module.exports = router;
