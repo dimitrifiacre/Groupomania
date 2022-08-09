3cd990da9ed000c1806cf2ae6e4b285b5dac67da
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-09 19:03:06 +0200

    Function to add like

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -1,5 +1,33 @@
 const db = require("../models");
 
-exports.addLike = async (req, res) => {};
+exports.addLike = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const postId = req.params.id;
+    const userExist = await db.User.findOne({ where: { user_id: userId } });
+    const postExist = await db.Post.findOne({ where: { post_id: postId } });
+    const likeExist = await db.Like.findOne({ where: { post_id: postId, user_id: userId } });
+
+    if (postExist) {
+      if (userExist) {
+        if (!likeExist) {
+          await db.Like.create({
+            post_id: postId,
+            user_id: userId,
+          });
+          return res.status(201).json({ message: "Un j'aime a été ajouté" });
+        } else {
+          return res.status(401).json({ error: "La publication a déjà été aimée" });
+        }
+      } else {
+        return res.status(404).json({ error: "L'utilisateur n'a pas été trouvé" });
+      }
+    } else {
+      return res.status(404).json({ error: "La publication n'a pas été trouvée" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 exports.removeLike = async (req, res) => {};
