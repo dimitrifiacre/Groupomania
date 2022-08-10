555b6b0f40067f5495aa9f763febc208aa2972df
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-10 11:07:48 +0200

    Adding multer in updateUser route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const { getOneUser, updateUser, deleteUser } = require("../controllers/user");
 const auth = require("../middleware/authJwt");
+const multer = require("../middleware/multerConfig");
 
 router.get("/:id", auth, getOneUser);
-router.put("/:id", auth, updateUser);
+router.put("/:id", auth, multer, updateUser);
 router.delete("/delete/:id", auth, deleteUser);
 
 module.exports = router;
