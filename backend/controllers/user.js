e27dfbf8d58e6768d0ee6bc2b10cb588e8c41d09
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-10 10:19:45 +0200

    Function to get one user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,19 @@
 const db = require("../models");
 
-exports.getOneUser = async (req, res) => {};
+exports.getOneUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const userExist = await db.User.findOne({ where: { user_id: userId } });
+    if (userExist) {
+      const user = await db.User.findOne({ where: { user_id: userId }, attributes: ["user_id", "user_firstname", "user_lastname", "user_avatar_url", "user_job", "user_admin"] });
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({ error: "L'utilisateur n'a pas été trouvé" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 exports.updateUser = async (req, res) => {};
 
