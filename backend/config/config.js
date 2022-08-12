0c148128c441ca5c410ae0747ecd58e5d25bd551
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-12 16:09:02 +0200

    Dialect is now in .env file

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: process.env.DB_DIALECT,
     timezone: "Europe/Paris",
     dialectOptions: {
       timezone: "local",
