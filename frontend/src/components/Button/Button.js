f3c57d682c5ae91cd3016ad321f2ce117e84b0ec
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-19 14:26:20 +0200

    Add button component

---
+++ frontend/src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Button.scss";
+import iconSet from "../../fonts/selection.json";
+import IcomoonReact from "icomoon-react";
+
+const Button = ({ value, className, onClick, icon, color }) => {
+  return (
+    <button className={icon ? className + " btn-icon" : className} onClick={onClick}>
+      {icon ? <IcomoonReact iconSet={iconSet} size={14} icon={icon} color={color} /> : value}
+    </button>
+  );
+};
+
+export default Button;
