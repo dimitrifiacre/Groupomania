00232cdbbcdf91a381657aa56c0b28fafb1d0eb3
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-19 11:27:20 +0200

    Different routes with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <div>App.js</div>;
+  return (
+    <Router>
+      <Routes>
+        {/* Private routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Public routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
