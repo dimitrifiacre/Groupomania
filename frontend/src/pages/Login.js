ac5ab6e0d6ac8a37683e10b74b29cff94d18fc9b
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-22 14:27:37 +0200

    Login page completed

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LoginForm from "../components/Forms/LoginForm";
+import logo from "../assets/logo.png";
 
 const Login = () => {
+  document.title = "Se connecter – Groupomania";
+
   return (
-    <div>
-      <LoginForm />
+    <div className="container container--form">
+      <img src={logo} alt="Logo Groupomania" width="242px" />
+      <div className="card card--form">
+        <LoginForm />
+        <span className="text-center">
+          Vous n'avez pas de compte ? <Link to="/register">Inscrivez-vous</Link>
+        </span>
+      </div>
     </div>
   );
 };
