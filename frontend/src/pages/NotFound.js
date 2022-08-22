3ef3cbbb879b3b797c58528d13641818921c0693
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-22 17:42:11 +0200

    Error page completed

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./NotFound.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>Error 404</div>;
+  return (
+    <div className="container container--form">
+      <div className="error__title">404</div>
+      <span className="error__subtitle">Oups, cette page n'existe pas</span>
+      <Link className="btn btn-primary" to="/">
+        Retour à l'accueil
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
