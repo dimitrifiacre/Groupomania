56237ec2e691faa420ef11bdd42475da37d7010f
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-23 20:07:39 +0200

    Recover avatar of connected user to display it in navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import avatarImg from "../../assets/default-avatar.png";
 import Avatar from "../Avatar/Avatar";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const [imgSrc, setImgSrc] = useState("");
+  const userData = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    try {
+      const userAvatar = userData && userData.user_avatar_url;
+      if (userAvatar == null) {
+        setImgSrc(avatarImg);
+      } else {
+        setImgSrc(`${process.env.REACT_APP_API_URL}img/${userData.user_avatar_url}`);
+      }
+    } catch (error) {
+      throw error;
+    }
+  });
+
   return (
     <nav className="nav">
       <div className="nav__container">
@@ -12,7 +29,7 @@ const Navbar = () => {
           <div className="nav__logo"></div>
         </Link>
         <Link to="/profile">
-          <Avatar className="avatar avatar-small" img={avatarImg} />
+          <Avatar className="avatar avatar-small" img={imgSrc} />
         </Link>
       </div>
     </nav>
