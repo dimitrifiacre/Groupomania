46598a6b8cac35bb91e6d6898ec5829109ee529d
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-23 09:57:29 +0200

    New context to retrieve and store the userId of the connected user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserContext } from "./components/AppContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
@@ -7,18 +9,32 @@ import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 
 const App = () => {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const checkJwt = async () => {
+      axios
+        .get("api/auth/checkjwt")
+        .then((res) => setUserId(res.data))
+        .catch((err) => console.log(err));
+    };
+    checkJwt();
+  }, [userId]);
+
   return (
-    <Router>
-      <Routes>
-        {/* Private routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* Public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <UserContext.Provider value={userId}>
+      <Router>
+        <Routes>
+          {/* Private routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
   );
 };
 
