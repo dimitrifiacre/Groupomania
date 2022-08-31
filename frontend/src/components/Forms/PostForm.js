8ea8caf3811a29f5f51fb6db64997a4ee4d135f3
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-08-31 15:20:49 +0200

    User avatar in post form

--- frontend/src/components/Forms/PostForm.js
+++ frontend/src/components/Forms/PostForm.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Form.scss";
 import Button from "../Button/Button";
 import Alert from "../Alert/Alert";
 import iconSet from "../../fonts/selection.json";
 import IcomoonReact from "icomoon-react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createPost, getAllPosts } from "../../store/actions/postActions";
+import Avatar from "../Avatar/Avatar";
+import avatarImg from "../../assets/default-avatar.png";
+import { isEmpty } from "../Utils";
 
 const NewPost = () => {
   const dispatch = useDispatch();
   const [content, setContent] = useState("");
   const [file, setFile] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [userDataAvatar, setUserDataAvatar] = useState("");
+  const userData = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    if (!isEmpty(userData)) {
+      if (userData.user_avatar_url == null) {
+        setUserDataAvatar(avatarImg);
+      } else {
+        setUserDataAvatar(`${process.env.REACT_APP_API_URL}img/${userData.user_avatar_url}`);
+      }
+    }
+  }, [userData]);
 
   // Récupère l'image upload et la met dans le hook
   const changeImage = (e) => {
@@ -41,6 +56,7 @@ const NewPost = () => {
       {errorMessage && <Alert className="alert alert-error" value={errorMessage} />}
       <form className="post-group" onSubmit={handleNewPost}>
         <div className="input-group">
+          <Avatar className="avatar avatar-small" img={userDataAvatar} />
           <textarea className="input-text input-textarea" id="content" name="content" value={content} placeholder="Quoi de neuf ?" onChange={(e) => setContent(e.target.value)}></textarea>
           <input type="file" id="file" accept=".png,.jpg,.jpeg,.gif" onChange={changeImage} />
           <label className="input-file" htmlFor="file">
