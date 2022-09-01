302f16ef2a06fc16d06298929a00097ba25ee8cf
dimitrifiacre <dimitri.fiacre@hotmail.fr>
2022-09-01 13:15:42 +0200

    Remove update and delete comment function of v1

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const { createComment, updateComment, deleteComment } = require("../controllers/comment");
+const { createComment } = require("../controllers/comment");
 const { authJwt } = require("../middleware/authJwt");
 const { validContent } = require("../middleware/validInputs");
 
 router.post("/:id", authJwt, validContent, createComment);
-router.put("/:id", authJwt, validContent, updateComment);
-router.delete("/delete/:id", authJwt, deleteComment);
 
 module.exports = router;
